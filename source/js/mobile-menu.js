6276915d7b00643bbf854d8df53bc25d5fded7ac
Pawel <kachurovskiy.p@gmail.com>
2020-05-24 19:47:57 +0200

    внисит правки после проверки наставником

--- source/js/mobile-menu.js
+++ source/js/mobile-menu.js
@@ -2,6 +2,8 @@ var menuButton = document.querySelector(".menu-button");
 var closeButton = document.querySelector(".mobile-menu__close");
 var menu = document.querySelector(".mobile-menu");
 
+menu.classList.remove("mobile-menu--no-js");
+
 menuButton.addEventListener("click", function(evt) {
   evt.preventDefault();
   menu.classList.add("mobile-menu--opened");
